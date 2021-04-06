b280b428c7c4324bc3080455472437166374a471
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2021-04-06 10:22:17 +0200

    use release tarball

--- default.nix
+++ default.nix
@@ -2,12 +2,11 @@ with import <nixpkgs> { };
 
 rustPlatform.buildRustPackage rec {
   name = "xplr";
-  version = "1.0";
-  src = fetchTarball ("https://github.com/sayanarijit/xplr/tarball/main");
+  version = "0.3.1";
+  src = fetchTarball
+    ("https://github.com/sayanarijit/xplr/archive/refs/tags/v0.3.1.tar.gz");
   buildInputs = [ cargo ];
-
   checkPhase = "";
   cargoSha256 = "sha256-IyaYkHXmqXziXNK6uYU+XNNWA8a8S8cuMxkopps/9kk=";
-
 }
 
