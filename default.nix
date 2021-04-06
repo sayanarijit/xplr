65ddb0ee4b6ffa387faf3f678547fa2767899a12
Arijit Basu <sayanarijit@gmail.com>
2021-04-06 18:55:57 +0530

    Release 0.3.2

--- default.nix
+++ default.nix
@@ -4,9 +4,9 @@ with import <nixpkgs> { };
 
 rustPlatform.buildRustPackage rec {
   name = "xplr";
-  version = "0.3.1";
+  version = "0.3.2";
   src = fetchTarball
-    ("https://github.com/sayanarijit/xplr/archive/refs/tags/v0.3.1.tar.gz");
+    ("https://github.com/sayanarijit/xplr/archive/refs/tags/v0.3.2.tar.gz");
   buildInputs = [ cargo ];
   checkPhase = "";
   cargoSha256 = "sha256-IyaYkHXmqXziXNK6uYU+XNNWA8a8S8cuMxkopps/9kk=";
