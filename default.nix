e76c1a338395f2afb9c59bacd56fc97ce111369d
Arijit Basu <sayanarijit@gmail.com>
2021-04-12 09:29:04 +0530

    Fix exit error codes
    Also remove cucumber-rust (will try https://github.com/rust-rspec/rspec)
    
    Fixes: https://github.com/sayanarijit/xplr/issues/33

--- default.nix
+++ default.nix
@@ -4,9 +4,9 @@ with import <nixpkgs> {};
 
 rustPlatform.buildRustPackage rec {
   name = "xplr";
-  version = "0.4.1";
+  version = "0.4.2";
   src = fetchTarball
-    ("https://github.com/sayanarijit/xplr/archive/refs/tags/v0.4.1.tar.gz");
+    ("https://github.com/sayanarijit/xplr/archive/refs/tags/v0.4.2.tar.gz");
   buildInputs = [ cargo ];
   checkPhase = "";
   cargoSha256 = "0000000000000000000000000000000000000000000000000000";
--- tests/steps/mod.rs
+++
@@ -1,1 +0,0 @@
-pub mod key_bindings;
