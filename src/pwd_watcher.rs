c9aba8ca869c2090858e174cb3049c58c54781e9
Arijit Basu <sayanarijit@gmail.com>
2021-04-13 07:34:12 +0530

    Fix panic on permission denied
    Issue:
    $PWD watch service panics when visiting restricted directories.
    
    Fix:
    Log error instead of panic.

--- src/pwd_watcher.rs
+++ src/pwd_watcher.rs
@@ -18,10 +18,24 @@ pub fn keep_watching(
     let mut last_pwd = pwd.to_string();
     thread::spawn(move || loop {
         if let Ok(new_pwd) = rx_pwd_watcher.try_recv() {
-            watcher.unwatch(&last_pwd).unwrap();
+            watcher.unwatch(&last_pwd).unwrap_or_else(|e| {
+                tx_msg_in
+                    .send(Task::new(
+                        MsgIn::External(ExternalMsg::LogError(e.to_string())),
+                        None,
+                    ))
+                    .unwrap();
+            });
             watcher
                 .watch(&new_pwd, RecursiveMode::NonRecursive)
-                .unwrap();
+                .unwrap_or_else(|e| {
+                    tx_msg_in
+                        .send(Task::new(
+                            MsgIn::External(ExternalMsg::LogError(e.to_string())),
+                            None,
+                        ))
+                        .unwrap();
+                });
             last_pwd = new_pwd;
         } else {
             thread::sleep(Duration::from_secs(1));
