b64d6b59a51b8aa5f56551da5b6766a6037289b5
Arijit Basu <sayanarijit@gmail.com>
2021-05-11 01:37:40 +0530

    Remove dependency notify
    Using a whole crate just to watch `$PWD` for the last modification time
    was a overkill.

--- src/pwd_watcher.rs
+++ src/pwd_watcher.rs
@@ -1,51 +1,39 @@
 use crate::app::Task;
 use crate::app::{ExternalMsg, MsgIn};
 use anyhow::Result;
-use notify::{watcher, RecursiveMode, Watcher};
-use std::sync::mpsc::{channel, Receiver, Sender};
+use std::path::PathBuf;
+use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 use std::time::Duration;
+use std::time::SystemTime;
 
 pub fn keep_watching(
     pwd: &str,
     tx_msg_in: Sender<Task>,
     rx_pwd_watcher: Receiver<String>,
 ) -> Result<()> {
-    let (tx, rx) = channel();
-    let mut watcher = watcher(tx, Duration::from_secs(1))?;
-    watcher.watch(pwd, RecursiveMode::NonRecursive)?;
+    let mut pwd = PathBuf::from(pwd);
+    let mut last_modified = pwd
+        .metadata()
+        .and_then(|m| m.modified())
+        .unwrap_or_else(|_| SystemTime::now());
 
-    let mut last_pwd = pwd.to_string();
     thread::spawn(move || loop {
         if let Ok(new_pwd) = rx_pwd_watcher.try_recv() {
-            watcher.unwatch(&last_pwd).unwrap_or_else(|e| {
-                tx_msg_in
-                    .send(Task::new(
-                        MsgIn::External(ExternalMsg::LogError(e.to_string())),
-                        None,
-                    ))
-                    .unwrap_or_default();
-            });
-            watcher
-                .watch(&new_pwd, RecursiveMode::NonRecursive)
-                .unwrap_or_else(|e| {
-                    tx_msg_in
-                        .send(Task::new(
-                            MsgIn::External(ExternalMsg::LogError(e.to_string())),
-                            None,
-                        ))
-                        .unwrap_or_default();
-                });
-            last_pwd = new_pwd;
+            pwd = PathBuf::from(new_pwd);
         } else {
-            thread::sleep(Duration::from_secs(1));
-        }
+            let modified = pwd
+                .metadata()
+                .and_then(|m| m.modified())
+                .unwrap_or_else(|_| SystemTime::now());
 
-        if rx.try_recv().is_ok() {
-            let msg = MsgIn::External(ExternalMsg::ExplorePwdAsync);
-            tx_msg_in.send(Task::new(msg, None)).unwrap_or_default();
-        } else {
-            thread::sleep(Duration::from_secs(1));
+            if modified != last_modified {
+                let msg = MsgIn::External(ExternalMsg::ExplorePwdAsync);
+                tx_msg_in.send(Task::new(msg, None)).unwrap_or_default();
+                last_modified = modified;
+            } else {
+                thread::sleep(Duration::from_millis(1000));
+            };
         }
     });
     Ok(())
