42f1c603538372fe813ccaa30050e4cbcf0eb272
Arijit Basu <sayanarijit@gmail.com>
2021-05-11 02:06:15 +0530

    Optimize release binary size
    Also improve the $PWD watcher logic.

--- src/pwd_watcher.rs
+++ src/pwd_watcher.rs
@@ -5,7 +5,6 @@ use std::path::PathBuf;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 use std::time::Duration;
-use std::time::SystemTime;
 
 pub fn keep_watching(
     pwd: &str,
@@ -13,27 +12,28 @@ pub fn keep_watching(
     rx_pwd_watcher: Receiver<String>,
 ) -> Result<()> {
     let mut pwd = PathBuf::from(pwd);
-    let mut last_modified = pwd
-        .metadata()
-        .and_then(|m| m.modified())
-        .unwrap_or_else(|_| SystemTime::now());
+    let mut last_modified = pwd.metadata().and_then(|m| m.modified())?;
 
     thread::spawn(move || loop {
         if let Ok(new_pwd) = rx_pwd_watcher.try_recv() {
             pwd = PathBuf::from(new_pwd);
         } else {
-            let modified = pwd
-                .metadata()
+            pwd.metadata()
                 .and_then(|m| m.modified())
-                .unwrap_or_else(|_| SystemTime::now());
-
-            if modified != last_modified {
-                let msg = MsgIn::External(ExternalMsg::ExplorePwdAsync);
-                tx_msg_in.send(Task::new(msg, None)).unwrap_or_default();
-                last_modified = modified;
-            } else {
-                thread::sleep(Duration::from_millis(1000));
-            };
+                .map(|modified| {
+                    if modified != last_modified {
+                        let msg = MsgIn::External(ExternalMsg::ExplorePwdAsync);
+                        tx_msg_in.send(Task::new(msg, None)).unwrap_or_default();
+                        last_modified = modified;
+                    } else {
+                        thread::sleep(Duration::from_millis(1000));
+                    };
+                })
+                .unwrap_or_else(|e| {
+                    let msg = MsgIn::External(ExternalMsg::LogError(e.to_string()));
+                    tx_msg_in.send(Task::new(msg, None)).unwrap_or_default();
+                    thread::sleep(Duration::from_millis(1000));
+                })
         }
     });
     Ok(())
