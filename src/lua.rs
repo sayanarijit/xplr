6991db8c33ba44c3309c553bf1380004ce8d6d04
Arijit Basu <sayanarijit@gmail.com>
2021-06-05 08:02:34 +0530

    Fix gpg publishing attempt 2

--- src/lua.rs
+++ src/lua.rs
@@ -134,10 +134,11 @@ mod test {
         assert!(check_version("0.13.0", "foo path").is_ok());
         assert!(check_version("0.13.1", "foo path").is_ok());
         assert!(check_version("0.13.2", "foo path").is_ok());
+        assert!(check_version("0.13.3", "foo path").is_ok());
 
-        assert!(check_version("0.13.3", "foo path").is_err());
-        assert!(check_version("0.14.2", "foo path").is_err());
-        assert!(check_version("0.11.2", "foo path").is_err());
-        assert!(check_version("1.13.2", "foo path").is_err());
+        assert!(check_version("0.13.4", "foo path").is_err());
+        assert!(check_version("0.14.3", "foo path").is_err());
+        assert!(check_version("0.11.3", "foo path").is_err());
+        assert!(check_version("1.13.3", "foo path").is_err());
     }
 }
