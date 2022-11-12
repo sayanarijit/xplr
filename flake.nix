ca4f4f3f45a46a1c954fdc90c55addf913026aa8
Arijit Basu <sayanarijit@gmail.com>
2022-11-12 20:58:59 +0530

    Fix nix build second attempt

--- flake.nix
+++ flake.nix
@@ -2,25 +2,33 @@
   description = "xplr - A hackable, minimal, fast TUI file explorer";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs";
+    nix.url = "github:domenkozar/nix/relaxed-flakes";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        rec {
-          packages = flake-utils.lib.flattenTree {
-            xplr = pkgs.rustPlatform.buildRustPackage rec {
-              name = "xplr";
-              src = ./.;
-              cargoLock = {
-                lockFile = ./Cargo.lock;
-              };
+  outputs = { self, nixpkgs, nix, ... }:
+    let
+      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          xplr = pkgs.rustPlatform.buildRustPackage rec {
+            name = "xplr";
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
             };
           };
-          defaultPackage = packages.xplr;
-        }
-      );
+        });
+      defaultPackage = forAllSystems (system: self.packages.${system}.xplr);
+    };
 }
