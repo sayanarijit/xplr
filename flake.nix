a0c19025dd902662a6db886ae38575cbe7b264ba
Arijit Basu <sayanarijit@gmail.com>
2022-11-12 20:41:42 +0530

    Fix nix build

--- flake.nix
+++ flake.nix
@@ -2,20 +2,25 @@
   description = "xplr - A hackable, minimal, fast TUI file explorer";
 
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-    {
-      packages.x86_64-linux.default =
-        with import nixpkgs { system = "x86_64-linux"; };
-
-        rustPlatform.buildRustPackage rec {
-          name = "xplr";
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        rec {
+          packages = flake-utils.lib.flattenTree {
+            xplr = pkgs.rustPlatform.buildRustPackage rec {
+              name = "xplr";
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+              };
+            };
           };
-        };
-    };
+          defaultPackage = packages.xplr;
+        }
+      );
 }
