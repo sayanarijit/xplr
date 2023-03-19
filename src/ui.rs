3811cfa98916b7e9d05203ffcd7bf5cf67a86f00
Arijit Basu <sayanarijit@gmail.com>
2023-03-19 22:58:17 +0530

    Update deps

--- src/ui.rs
+++ src/ui.rs
@@ -5,7 +5,7 @@ use crate::config::PanelUiConfig;
 use crate::lua;
 use crate::permissions::Permissions;
 use crate::{app, path};
-use ansi_to_tui::IntoText;
+use ansi_to_tui_forked::IntoText;
 use indexmap::IndexSet;
 use lazy_static::lazy_static;
 use lscolors::{Color as LsColorsColor, Style as LsColorsStyle};
@@ -202,7 +202,7 @@ pub enum Border {
 }
 
 impl Border {
-    pub fn bits(self) -> u32 {
+    pub fn bits(self) -> u8 {
         match self {
             Self::Top => TuiBorders::TOP.bits(),
             Self::Right => TuiBorders::RIGHT.bits(),
@@ -257,7 +257,7 @@ pub enum Modifier {
 }
 
 impl Modifier {
-    pub fn bits(self) -> u16 {
+    pub fn bits(self) -> u8 {
         match self {
             Self::Bold => TuiModifier::BOLD.bits(),
             Self::Dim => TuiModifier::DIM.bits(),
@@ -314,7 +314,7 @@ impl Style {
 
 impl Into<TuiStyle> for Style {
     fn into(self) -> TuiStyle {
-        fn xor(modifiers: Option<IndexSet<Modifier>>) -> u16 {
+        fn xor(modifiers: Option<IndexSet<Modifier>>) -> u8 {
             modifiers
                 .unwrap_or_default()
                 .into_iter()
@@ -1378,7 +1378,7 @@ pub fn draw_layout<B: Backend>(
                 .into_iter()
                 .zip(chunks.into_iter())
                 .for_each(|(split, chunk)| {
-                    draw_layout(split, f, screen_size, chunk, app, lua)
+                    draw_layout(split, f, screen_size, *chunk, app, lua)
                 });
         }
 
@@ -1409,7 +1409,7 @@ pub fn draw_layout<B: Backend>(
                 .into_iter()
                 .zip(chunks.into_iter())
                 .for_each(|(split, chunk)| {
-                    draw_layout(split, f, screen_size, chunk, app, lua)
+                    draw_layout(split, f, screen_size, *chunk, app, lua)
                 });
         }
     }
