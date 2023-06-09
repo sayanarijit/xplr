4ccd9796c43c7e993baf033fb9a1538d306b08da
Noah Mayr <dev@noahmayr.com>
2023-06-09 18:58:28 +0200

    Use xdg-rust crate instead of dirs crate (#631)
    * Use xdg-rust crate instead of dirs crate
    
    * Fix clippy warning

---
+++ src/dirs.rs
@@ -0,0 +1,25 @@
+use std::{env, path::PathBuf};
+
+use lazy_static::lazy_static;
+use xdg::BaseDirectories;
+
+lazy_static! {
+    pub static ref BASE_DIRS: Option<BaseDirectories> = BaseDirectories::new().ok();
+}
+
+pub fn home_dir() -> Option<PathBuf> {
+    home::home_dir()
+}
+
+pub fn config_dir() -> Option<PathBuf> {
+    BASE_DIRS.as_ref().map(|base| base.get_config_home())
+}
+
+pub fn runtime_dir() -> PathBuf {
+    let Some(dir) = BASE_DIRS
+        .as_ref()
+        .and_then(|base| base.get_runtime_directory().ok()) else {
+        return env::temp_dir();
+    };
+    dir.to_owned()
+}
--- src/path.rs
+++ src/path.rs
@@ -1,3 +1,4 @@
+use crate::dirs;
 use anyhow::{bail, Result};
 use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
