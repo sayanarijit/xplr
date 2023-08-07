22b5fca8d90e450ee52a901f1ec6fdb9dbb9ebb8
Arijit Basu <sayanarijit@gmail.com>
2023-08-07 23:33:20 +0530

    Update version

--- src/lua/mod.rs
+++ src/lua/mod.rs
@@ -160,24 +160,24 @@ mod tests {
         assert!(check_version(VERSION, "foo path").is_ok());
 
         // Current release if OK
-        assert!(check_version("0.21.2", "foo path").is_ok());
+        assert!(check_version("0.21.3", "foo path").is_ok());
 
         // Prev major release is ERR
         // - Not yet
 
         // Prev minor release is ERR (Change when we get to v1)
-        assert!(check_version("0.20.2", "foo path").is_err());
+        assert!(check_version("0.20.3", "foo path").is_err());
 
         // Prev bugfix release is OK
-        assert!(check_version("0.21.1", "foo path").is_ok());
+        assert!(check_version("0.21.2", "foo path").is_ok());
 
         // Next major release is ERR
-        assert!(check_version("1.20.2", "foo path").is_err());
+        assert!(check_version("1.20.3", "foo path").is_err());
 
         // Next minor release is ERR
-        assert!(check_version("0.22.2", "foo path").is_err());
+        assert!(check_version("0.22.3", "foo path").is_err());
 
         // Next bugfix release is ERR (Change when we get to v1)
-        assert!(check_version("0.21.3", "foo path").is_err());
+        assert!(check_version("0.21.4", "foo path").is_err());
     }
 }
